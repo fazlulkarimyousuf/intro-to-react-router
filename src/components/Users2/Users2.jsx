6d0f4328195ba58cd2a24c8c4e79e87c042d2270
fazlulkarimyousuf <faizmiah52@gmail.com>
2025-04-27 15:27:24 +0600

    add the file

--- src/components/Users2/Users2.jsx
+++ src/components/Users2/Users2.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {use} from 'react';
 
-const Users2 = () => {
+const Users2 = ({usersPromise}) => {
+    const users = use(usersPromise)
+
+    console.log('usersPromise', users);
+    
+    
     return (
         <div>
-            
+            <h2>This is users2</h2>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import {createBrowserRouter,RouterProvider,} from "react-router";
 import Root from './components/Root/Root.jsx';
 import Home from './components/Home/Home.jsx';
 import Mobiles from './components/Mobiles/Mobiles.jsx';
@@ -13,6 +10,8 @@ import Laptops from './components/Laptops/Laptops.jsx';
 import Users from './components/Users/Users.jsx';
 import Users2 from './components/Users2/Users2.jsx';
 
+
+const usersPromise = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
 const router = createBrowserRouter([
   {
@@ -30,9 +29,10 @@ const router = createBrowserRouter([
         {
           path: 'users2',
           element: <Suspense fallback={<span>Loading......</span>}>
-            <Users2 ></Users2>
+            <Users2 usersPromise={usersPromise}></Users2>
           </Suspense>,},
-    ]
+          ]
+
   },
   {
     path: "about",
